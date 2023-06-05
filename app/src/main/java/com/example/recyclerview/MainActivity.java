5e28320c78a5970a61d4df5d1c12bd5fab41881b
Blue Teeth <daominhtri1000@gmail.com>
2023-06-06 06:52:15 +0700

    Create Project RecyclerView

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
